ad0347fd34c3129a5f43ad84e797d8c0fdaa6900
Felipe Guimarães <fguiengenharia@gmail.com>
2021-09-21 09:21:45 -0300

    PHP level one - Add uma string em um arquivo ao mesmo tempo preservando seu conteúdo

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
-    $nome = 'Fulano da Silva';
+    //lendo o arquivo
+    $text = file_get_contents('text.txt');
 
-    /*o file_put_contents: cria um novo arquivo com o conteúdo passado
-      caso o arquivo já exista, ele vai apenas substituir o texto que já
-      estava anteriormente
+    //add um conteúdo no arquivo que está sendo lido
+    /*
+    OBS: note que, a variável text ela está sendo concatenada
+         e ao mesmo tempo sendo atribuído um valor nela, que
+         no caso é mais uma string
     */
-    file_put_contents('nome.txt', $nome);
+    $text .= "\nBeltrano da Costa Farias";
 
-    echo 'Arquivo criado com sucesso!';
+    //escrevendo no arquivo
+    file_put_contents('text.txt', $text);
+
+    echo "Arquivo escrito com sucesso!";
 ?>
